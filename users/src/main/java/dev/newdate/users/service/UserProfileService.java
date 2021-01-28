e5a3ba872f0e44feb8ffde55523ac6d7255f8fa8
Angel Juarez <angeljuarez729@gmail.com>
2021-01-28 16:41:57 -0500

    renamed users -> server

--- users/src/main/java/dev/newdate/users/service/UserProfileService.java
+++
@@ -1,8 +0,0 @@
-package dev.newdate.users.service;
-
-import dev.newdate.users.model.UserProfile;
-
-public interface UserProfileService {
-    UserProfile createUserProfile(String username, UserProfile userProfile);
-    UserProfile getUserProfile(String username);
-}
